e5274136bbd8e1c0fbba3e97f5616b697e201135
Søren Roug <soren.roug@eea.europa.eu>
2020-04-14 15:49:25 +0200

    Fixed obsolete syntax.

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,13 +1,13 @@
 
-#VERS=1.0
+VERS=1.3
 #BUILDTIME=$(date '+%Y-%m-%dT%H%M')
 LATEST=latest
 NAME=eeacms/subversion
 
 docker build -t ${NAME}:${LATEST} .
-#docker tag -f ${NAME} ${NAME}:${VERS}
-#docker tag -f ${NAME} ${NAME}:${BUILDTIME}
+docker tag ${NAME} ${NAME}:${VERS}
+#docker tag ${NAME} ${NAME}:${BUILDTIME}
 docker push ${NAME}:${LATEST}
-#docker push ${NAME}:${VERS}
+docker push ${NAME}:${VERS}
 #docker push ${NAME}:${BUILDTIME}
 
